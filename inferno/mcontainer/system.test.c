1671bbe9e7cc2a85eb42a5dcbb086472c8c5d5ec
Matthew Temple <clownfysh@gmail.com>
2010-07-20 12:34:17 -0400

    removed old stub projects from inferno

--- inferno/mcontainer/system.test.c
+++
@@ -1,6 +0,0 @@
-#include "inferno/mcontainer/system.h"
-
-int main(int argc, char *argv[])
-{
-  return 0;
-}
--- inferno/mnet/system.test.c
+++
@@ -1,6 +0,0 @@
-#include "inferno/mnet/system.h"
-
-int main(int argc, char *argv[])
-{
-  return 0;
-}
